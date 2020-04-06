c7709948d9383eb2ee8527d42dd30ae33b45481b
cswithterrence <62587965+cswithterrence@users.noreply.github.com>
2020-04-06 00:57:10 -0400

    Add files via upload

---
+++ Sizeof Data Types.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("int: %Id \n", sizeof(int));
+    printf("float: %Id \n", sizeof(float));
+    printf("double: %Id \n", sizeof(double));
+    printf("char: %Id \n", sizeof(char));
+    
+    return 0;
+}
